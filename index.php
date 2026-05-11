28ef89eac2bc96cf86010f4b16db7faf95048d8e
Przemek Wojtyczka <przemo972@gmai.com>
2026-05-11 10:17:54 +0200

    dodanie możliwości wykonywania przelewów

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
     require_once 'src/konta_bankowe.php';
 
+    echo "Założenie konta osobistego:\n";
     $konto_osobiste = new KontoOsobiste("1234567890", 1000.00, "Jan Kowalski");
     echo $konto_osobiste->daneKonta() . "\n";
     $konto_osobiste->wplata(500.00);
-    $konto_osobiste->wyplata(200.00);
+    $konto_osobiste->wyplata(1000.00);
+
+    echo "\nZałożenie konta firmowego:\n";
+    $konto_firmowe = new KontoFirmowe("0987654321", 3000.00, "Firma XYZ", 1000.00);
+    echo $konto_firmowe->daneKonta() . "\n";
+    $konto_firmowe->wplata(1000.00);
+    $konto_firmowe->wyplata(4500.00);
+    
+    echo"\nPrzelew z konta firmowego na konto osobiste:\n";
+    $konto_firmowe->przelew($konto_osobiste, 600.00);
+
+    echo "\nPrzelew z konta osobistego na konto firmowe:\n";
+    $konto_osobiste->przelew($konto_firmowe, 300.00);
+    
+    echo"\nPrzelew z konta firmowego na konto osobiste:\n";
+    $konto_firmowe->przelew($konto_osobiste, 600.00);
+
+    echo "\nStan kont:\n";
+    echo $konto_osobiste->daneKonta() . "\n";
+    echo $konto_firmowe->daneKonta() . "\n";
--- src/konta_bankowe.php
+++ src/konta_bankowe.php
@@ -44,8 +44,46 @@ abstract class Konto {
             echo "Nie można wypłacić tej kwoty. Sprawdź saldo lub wprowadź poprawną kwotę.\n";
         }
     }
+    
+    public function przelew(Konto $kontoDocelowe, float $kwota) {
+        if ($kwota > 0 && $kwota <= $this->saldo) {
+            $this->wyplata($kwota);
+            $kontoDocelowe->wplata($kwota);
+            echo "Przelew z konta " . $this->numerKonta . " na konto " . $kontoDocelowe->numerKonta() . " został wykonany. Kwota: " . $kwota . " zł.\n";
+        } else {
+            echo "Nie można wykonać przelewu. Sprawdź saldo lub wprowadź poprawną kwotę.\n";
+        }
+    }
 }
 
 class KontoOsobiste extends Konto {
 
+}
+
+class KontoFirmowe extends Konto {
+    private float $debet;
+
+    public function __construct(string $numerKonta, float $saldo, string $wlasciciel, float $debet) {
+        parent::__construct($numerKonta, $saldo, $wlasciciel);
+        $this->debet = $debet;
+    }
+
+    public function wyplata($kwota) {
+        if ($kwota > 0 && $kwota <= ($this->saldo + $this->debet)) {
+            $this->saldo -= $kwota;
+            echo "Numer konta: " . $this->numerKonta . ", Wypłacono: " . $kwota . " zł. Nowe saldo: " . $this->saldo . " zł.\n";
+        } else {
+            echo "Nie można wypłacić tej kwoty. Sprawdź saldo, debet lub wprowadź poprawną kwotę.\n";
+        }
+    }
+    
+    public function przelew(Konto $kontoDocelowe, float $kwota) {
+        if ($kwota > 0 && $kwota <= ($this->saldo + $this->debet)) {
+            $this->wyplata($kwota);
+            $kontoDocelowe->wplata($kwota);
+            echo "Przelew z konta " . $this->numerKonta . " na konto " . $kontoDocelowe->numerKonta() . " został wykonany. Kwota: " . $kwota . " zł.\n";
+        } else {
+            echo "Nie można wykonać przelewu. Sprawdź saldo lub wprowadź poprawną kwotę.\n";
+        }
+    }
 }
